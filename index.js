b85000b76d4baaa680b605131e70ce53a050f28f
Ismael Gonçalves da Silva <ismaelgj123567@gmail.com>
2023-02-10 16:39:11 -0300

    3

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 const  readline = require( 'readline-sync' ) ; 
 const chalk = require('chalk');
 
-const yellow = chalk.yellow;
-const white = chalk.underline.bold.black.bgWhite;
-const green = chalk.green;
-const blue = chalk.blue;
-const ciano = chalk.cyan;
 const array = []
 let item = ''
 
-console.log(white(`\n\nLista de Propriedades do CSS, adicione alguns itens na lista:\nPara ver a lista, digite "ver lista"\nPara sair digite sair`))
+console.log((`\n\nLista de Propriedades do CSS, adicione alguns itens na lista:\nPara ver a lista, digite "ver lista"\nPara sair digite sair`))
 
 while( item.toLowerCase() != 'sair'){
     
-    item = readline.question(yellow(`Digite as propriedades do ${ciano("CSS")}:`))
+    item = readline.question(chalk.yellow(`Digite as propriedades do ${chalk.cyan("CSS")}:`))
     if( item.toLowerCase() != 'sair' && item.toLowerCase() != 'ver lista' ){
 
         array.push(item.toLowerCase())
     }
     else if( item.toLowerCase() === 'ver lista'){
-        console.log(`\n\n ${blue("Lista de propriedades CSS Ordenada de A-Z:")} \n\n` + green(array.sort().join('\n')))
+        console.log(`\n\n ${chalk.blue("Lista de propriedades CSS Ordenada de A-Z:")} \n\n` + chalk.green(array.sort().join('\n')))
     }
     else{
-        console.log(`\n\n ${blue("Lista de propriedades CSS Ordenada de A-Z:")} \n\n` + green(array.sort().join('\n')))
+        console.log(`\n\n ${chalk.blue("Lista de propriedades CSS Ordenada de A-Z:")} \n\n` + chalk.green(array.sort().join('\n')))
     }
 }
